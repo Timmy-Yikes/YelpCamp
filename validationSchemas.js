73e11799082f96887749fc38c989b13840d22de8
Tian Yu <timmy.yikes01@gmail.com>
2022-09-15 22:40:27 -0700

    Security issues

--- validationSchemas.js
+++ validationSchemas.js
@@ -1,22 +1,47 @@
 // Individual Joi helpers to validate single documents of different models
 let Joi = require('joi');
+let sanitizeHTML = require('sanitize-html');
+
+let extension = (joi) => {
+    return {
+        type: 'string',
+        base: joi.string(),
+        message: {
+            'string.escapeHTML': '{{#label}} must not include HTML!'
+        },
+        rules: {
+            escapeHTML: {
+                validate(value, helpers) {
+                    let clean = sanitizeHTML(value, {
+                        allowedTags: [],
+                        allowedAttributes: {}
+                    });
+                    if (clean !== value) return helpers.error('string.escapeHTML', {value});
+                    return clean;
+                }
+            }
+        }
+    }
+}
+
+Joi = Joi.extend(extension);
 
 module.exports.campgroundSchema = Joi.object({
-    name: Joi.string().required(),
+    name: Joi.string().required().escapeHTML(),
     images: Joi.array().items(Joi.object({
         url: Joi.string(),
         filename: Joi.string()
     })),
     price: Joi.number().min(0).required(),
-    description: Joi.string().required(),
-    location: Joi.string().required(),
+    description: Joi.string().required().escapeHTML(),
+    location: Joi.string().required().escapeHTML(),
     geometry: Joi.array(),
-    author: Joi.string().required(),
+    author: Joi.string().required().escapeHTML(),
     deleteImages: Joi.array()
 });
 
 module.exports.reviewSchema = Joi.object({
-    text: Joi.string().required(),
+    text: Joi.string().required().escapeHTML(),
     rating: Joi.number().min(0).max(5).required(),
     campground: [
         Joi.string(),
@@ -26,8 +51,8 @@ module.exports.reviewSchema = Joi.object({
 })
 
 module.exports.userSchema = Joi.object({
-    username: Joi.string().min(3).required(),
+    username: Joi.string().min(3).required().escapeHTML(),
     email: Joi.string().email(),
-    password: Joi.string().min(8).required(),
+    password: Joi.string().min(8).required().escapeHTML(),
     repeat_password: Joi.ref('password'),
 });
